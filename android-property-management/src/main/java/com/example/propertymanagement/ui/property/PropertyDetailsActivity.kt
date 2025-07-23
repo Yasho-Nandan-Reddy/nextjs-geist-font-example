98aa2508bfebfa3a8f24255511be287f7ad74b89
Yashonandan Reddy <141211604+Yasho-Nandan-Reddy@users.noreply.github.com>
2025-07-23 17:06:34 +0530

    Add android-property-management/src/main/java/com/example/propertymanagement/ui/property/PropertyDetailsActivity.kt 1753270594184

---
+++ android-property-management/src/main/java/com/example/propertymanagement/ui/property/PropertyDetailsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.propertymanagement.ui.property
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.propertymanagement.R
+
+class PropertyDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_property_details)
+
+        // TODO: Implement logic to display tenant info, lease duration, rent due status
+    }
+}
