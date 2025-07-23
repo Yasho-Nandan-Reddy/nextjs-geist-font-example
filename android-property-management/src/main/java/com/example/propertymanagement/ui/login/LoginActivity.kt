bdeee505c8b2ae6b77cda1ddae206f4cae3cb9af
Yashonandan Reddy <141211604+Yasho-Nandan-Reddy@users.noreply.github.com>
2025-07-23 17:06:39 +0530

    Add android-property-management/src/main/java/com/example/propertymanagement/ui/login/LoginActivity.kt 1753270599242

---
+++ android-property-management/src/main/java/com/example/propertymanagement/ui/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.propertymanagement.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.propertymanagement.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // TODO: Implement login logic and navigation to dashboards
+    }
+}
